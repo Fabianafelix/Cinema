7fee0c68f1c344e1069a02c3280e19d08f58b584
FELIX <fabiana.felix@vidanova.local>
2022-07-06 18:11:50 -0300

    Alteração no código

---
+++ Cinema..py
@@ -0,0 +1,43 @@
+lugares=int(input("\nCINEMA VIDA NOVA \n\nDigite o total de lugares disponíveis: "))
+posicao=()
+
+vetor = [posicao]*lugares
+print (vetor)
+
+while True:
+    assento = int(input("\nEscolha seu assento: "))
+
+    if (assento <= lugares):
+        if (vetor [assento-1]) == "X":
+            print ("Assento número", assento, "está ocupado. Escolha outro.")
+   
+        else:
+           vetor [assento-1] = "X"
+           print ("Assento número", assento, "reservado com sucesso.")
+     
+    else:
+        print("Esse assento não existe. Escolha um assento válido.")
+    
+    print(vetor)
+
+    
+  
+
+
+
+
+
+
+
+ 
+
+
+
+
+
+
+
+
+
+
+
